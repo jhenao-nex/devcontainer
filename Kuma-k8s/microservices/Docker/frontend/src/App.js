75c1457a37b97571b7be50e63906f3ae5045f051
jhenao-nex <jaime.andres.henao.arbelaez@ba.com>
2024-10-14 22:08:52 -0500

    commit to develop

---
+++ Kuma-k8s/microservices/Docker/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>¡Hola, Mundo!</h1>
+            <p>Esta es mi aplicación React.</p>
+        </div>
+    );
+}
+
+export default App;
